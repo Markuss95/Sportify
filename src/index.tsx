986a9165dd099725695d3dd82ad740795fa9aa3b
unknown <dreykey94@gmail.com>
2021-05-08 11:27:43 +0200

    first real commit

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+ReactDOM.render(<App />, document.getElementById("root"));
